b1a5eb202f2a7c46d7e851f09c2e2a3630ba5a17
vishalsvaghasiya <vishal.genextwebs@gmail.com>
2021-08-11 16:22:48 +0530

    Working Jetstream

---
+++ app/Models/ChatMessage.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChatMessage extends Model
+{
+    use HasFactory;
+
+    public function room(){
+        return $this->hasOne('App\Models\ChatRoom' , 'id' , 'chat_room_id');
+    }
+}
---
+++ resources/js/Pages/Chat/container.vue
@@ -0,0 +1,19 @@
+<template>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                container
+            </h2>
+
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout.vue'
+    import Welcome from '@/Jetstream/Welcome.vue'
+
+    export default {
+        name:'container',
+        components: {
+            AppLayout,
+            Welcome,
+        },
+    }
+</script>
